161e612c2a5bfae11ba2dfcd9581f484e060269a
ManhTien <ninhmanh.tien@nomovok.vn>
2014-04-24 11:49:42 +0700

    Add File

--- javascript/Onclick.js
+++ javascript/Onclick.js
@@ -1,10 +1,32 @@
 // JavaScript Document
+'use strict';
+var clickHandler = {
+	'button1' : function(){
+		window.parent.alert("Hello Alert");
+	},
+	
+	'button2' : function(){
+	   window.parent.alert(prompt("Type text",'initial value'));	
+	},
+	'buttonNotify' : function(){
+		var Notification;
+		
+		Notification = window.Notification;
+		
+		var notification  = new Notification('Test Notification',{
+			body: 'Hello Notification'
+		});
+		
+	}
+};
 
- function Login()
-   {
-      window.open("index2.html","_self")
-   }
- function Back()
-   {
-      window.open("index.html","_self")
-   }
+document.body.addEventListener('click',function(evt){
+if(clickHandler[evt.target.id])
+         clickHandler[evt.target.id].call(this,evt);
+});
+
+
+function myFunction()
+{
+document.body.getElementsByClassName("demo").innerHTML  = "Love Me? OK?";
+}
